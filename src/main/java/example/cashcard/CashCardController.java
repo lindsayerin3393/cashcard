1a29e67f022537badde8c5c3536d43ed4ef34ced
Lindsay Powell <lindsayerin3393@gmail.com>
2023-06-07 11:27:39 -0400

    delete added

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,31 +1,21 @@
 package example.cashcard;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import java.util.List;
-import java.net.URI;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 import java.security.Principal;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
   private CashCardRepository cashCardRepository;
-  private CashCard cashCardUpdate;
-
-  private CashCard findCashCard(Long requestedId, Principal principal) {
-    return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
-  }
 
   public CashCardController(CashCardRepository cashCardRepository) {
     this.cashCardRepository = cashCardRepository;
@@ -52,8 +42,8 @@ public class CashCardController {
   }
 
   @GetMapping
-  public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
-    Page<CashCard> page = cashCardRepository.findAll(
+  public ResponseEntity<List<CashCard>> findAll(Pageable pageable, Principal principal) {
+    Page<CashCard> page = cashCardRepository.findByOwner(principal.getName(),
         PageRequest.of(
             pageable.getPageNumber(),
             pageable.getPageSize(),
@@ -66,7 +56,7 @@ public class CashCardController {
       Principal principal) {
     CashCard cashCard = findCashCard(requestedId, principal);
     if (cashCard != null) {
-      CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+      CashCard updatedCashCard = new CashCard(requestedId, cashCardUpdate.amount(), principal.getName());
       cashCardRepository.save(updatedCashCard);
       return ResponseEntity.noContent().build();
     }
@@ -74,7 +64,15 @@ public class CashCardController {
   }
 
   @DeleteMapping("/{id}")
-  private ResponseEntity<Void> deleteCashCard(@PathVariable Long id) {
-    return ResponseEntity.noContent().build();
+  private ResponseEntity<Void> deleteCashCard(@PathVariable Long id, Principal principal) {
+    if (cashCardRepository.existsByIdAndOwner(id, principal.getName())) {
+      cashCardRepository.deleteById(id);
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  private CashCard findCashCard(Long requestedId, Principal principal) {
+    return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
   }
 }
--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -7,24 +7,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
   @Autowired
   TestRestTemplate restTemplate;
 
   @Test
-  @DirtiesContext
   void shouldReturnACashCardWhenDataIsSaved() {
     ResponseEntity<String> response = restTemplate
         .withBasicAuth("sarah1", "abc123")
@@ -170,6 +168,7 @@ class CashCardApplicationTests {
         .withBasicAuth("sarah1", "abc123")
         .exchange("/cashcards/99", HttpMethod.PUT, request, Void.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
     ResponseEntity<String> getResponse = restTemplate
         .withBasicAuth("sarah1", "abc123")
         .getForEntity("/cashcards/99", String.class);
@@ -198,5 +197,30 @@ class CashCardApplicationTests {
         .withBasicAuth("sarah1", "abc123")
         .exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    ResponseEntity<String> getResponse = restTemplate
+        .withBasicAuth("sarah1", "abc123")
+        .getForEntity("/cashcards/99", String.class);
+    assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
   }
+
+  @Test
+  void shouldNotDeleteACashCardThatDoesNotExist() {
+    ResponseEntity<Void> deleteResponse = restTemplate
+        .withBasicAuth("sarah1", "abc123")
+        .exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+    assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  void shouldNotAllowDeletionOfCashCardsTheyDoNotOwn() {
+    ResponseEntity<Void> deleteResponse = restTemplate
+        .withBasicAuth("sarah1", "abc123")
+        .exchange("/cashcards/102", HttpMethod.DELETE, null, Void.class);
+    assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    ResponseEntity<String> getResponse = restTemplate
+        .withBasicAuth("kumar2", "xyz789")
+        .getForEntity("/cashcards/102", String.class);
+    assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
+
 }
